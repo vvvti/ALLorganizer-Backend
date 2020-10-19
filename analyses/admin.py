97737e99323797e30a4a27d5090a1cf8f0c43e09
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-19 20:11:10 +0200

    ALL-5 - Create and migrate models for analyses app

--- analyses/admin.py
+++ analyses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CompleteBloodCount, CrpAnalysis, UrineAnalisis, ALT_AST
 
-# Register your models here.
+admin.site.register(CompleteBloodCount)
+admin.site.register(CrpAnalysis)
+admin.site.register(UrineAnalisis)
+admin.site.register(ALT_AST)
+
--- analyses/models.py
+++ analyses/models.py
@@ -1,3 +1,129 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class CrpAnalysis(models.Model):  
+    crp_date = models.DateTimeField(
+        "data i godzina badania", default=timezone.now)
+    crp_count = models.DecimalField(
+        "Wynik CRP", max_digits=4, decimal_places=2)
+    is_active = models.BooleanField
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-crp_date',)
+        verbose_name = "Badanie CRP"
+        verbose_name_plural = "Badania CRP"
+
+    def __str__(self):
+        return str(self.crp_date)
+
+
+class CompleteBloodCount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cbc_date = models.DateTimeField(
+        "Data i godzina badania", default=timezone.now)
+    leucocyte_count = models.DecimalField(
+        "Liczba leukocytów", max_digits=8, decimal_places=2)
+    erythrocyte_count = models.DecimalField(
+        "Erytrocyty", blank=True, null=True, max_digits=4, decimal_places=2)
+    hemoglobin = models.DecimalField(
+        "Hemoglobina", max_digits=3, decimal_places=1)
+    hematocrit = models.DecimalField(
+        "Hematokryt", max_digits=3, decimal_places=1)
+    mcv = models.DecimalField(
+        "MCV", blank=True, null=True, max_digits=4, decimal_places=1)
+    mch = models.DecimalField(
+        "MCH", blank=True, null=True, max_digits=3, decimal_places=1)
+    mchc = models.DecimalField(
+        "MCHC", blank=True, null=True, max_digits=3, decimal_places=1)
+    thrombocyte_count = models.DecimalField(
+        "Płytki krwi", max_digits=3, decimal_places=0)
+    rdw_sd = models.DecimalField(
+        "RDW-SD", blank=True, null=True, max_digits=3, decimal_places=1)
+    rdw_cv = models.DecimalField(
+        "RDW-CV", blank=True, null=True, max_digits=3, decimal_places=1)
+    pdw = models.DecimalField(
+        "PDW", blank=True, null=True, max_digits=3, decimal_places=1)
+    mpv = models.DecimalField(
+        "MPV", blank=True, null=True, max_digits=3, decimal_places=1)
+    p_lcr = models.DecimalField(
+        "P-LCR", blank=True, null=True, max_digits=3, decimal_places=1)
+    neutrophils = models.DecimalField(
+        "Neutrofile", max_digits=3, decimal_places=1)
+    lymphocytes = models.DecimalField(
+        "Limfocyty", blank=True, null=True, max_digits=3, decimal_places=1)
+    monocytes = models.DecimalField(
+        "Monocyty", blank=True, null=True, max_digits=3, decimal_places=1)
+    eosinophils = models.DecimalField(
+        "Eozynofile", blank=True, null=True, max_digits=3, decimal_places=1)
+    baso = models.DecimalField(
+        "Bazofile", blank=True, null=True, max_digits=3, decimal_places=1)
+    notes = models.CharField("Uwagi", max_length=200, blank=True)    
+    is_active = models.BooleanField
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-cbc_date',)
+        verbose_name = "Morfologia krwi"
+        verbose_name_plural = "Morfologie krwi"
+
+    def __str__(self):
+        return str(self.cbc_date)
+
+class ALT_AST(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    alt_ast_date = models.DateTimeField(
+        "Data i godzina badania", default=timezone.now,)
+    alt = models.DecimalField("ALT", max_digits=4, decimal_places=0)
+    ast = models.DecimalField("AST", max_digits=4, decimal_places=0)    
+    is_active = models.BooleanField
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-alt_ast_date',)
+        verbose_name = "Badanie ALT, AST"
+        verbose_name_plural = "Badania ALT, AST"
+
+    def __str__(self):
+        return str(self.alt_ast_date)
+
+
+class UrineAnalisis(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    urine_date = models.DateTimeField(
+        "Data i godzina badania", default=timezone.now,)
+    color = models.CharField("Kolor", max_length=45)
+    specific_gravity = models.DecimalField(
+        "Ciężar właściwy", max_digits=5, decimal_places=4)
+    ph = models.DecimalField("PH", max_digits=3, decimal_places=1)
+    gluccose = models.CharField(
+        "Glukoza", max_length=45, blank=True)
+    ketone_bodies = models.CharField(
+        "Ketony", max_length=45, blank=True)
+    urobilinogen = models.CharField(
+        "Urobilinogen", max_length=45, blank=True)
+    bilurbin = models.CharField(
+        "Bilurbina", max_length=45, blank=True)
+    protein = models.CharField("Białko", max_length=45, blank=True)
+    erythrocyte = models.CharField(
+        "Erytrocyty", max_length=45, blank=True)
+    leucocyte = models.CharField(
+        "Leukocyty", max_length=45, blank=True)
+    bacterial = models.CharField(
+        "Bakterie", max_length=45, blank=True)
+    notes = models.CharField("Uwagi", max_length=200, blank=True)    
+    is_active = models.BooleanField
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-urine_date',)
+        verbose_name = "Badanie Moczu"
+        verbose_name_plural = "Badania Moczu"
+
+    def __str__(self):
+        return str(self.urine_date)
